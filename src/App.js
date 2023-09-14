0be88440fc7771b9e6149cbfcabd2082e7702086
a11arjunyadav <yadavarjun0312@gmail.com>
2023-09-14 18:09:47 +0530

    c2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
  // 1. Imaport area
-import logo from './logo.svg';
-import './App.css';
 import Header from './Components/Header';
 import Aside from './Components/Aside';
 import Section from './Components/Section';
